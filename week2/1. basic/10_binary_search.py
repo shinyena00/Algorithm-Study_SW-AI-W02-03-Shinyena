a0ebd8db29fbc85f7fd74606eb0c50358437cd65
shinyena00 <syn77@pusan.ac.kr>
2026-08-28 20:51:26 +0900

    10번 끝

--- week2/1. basic/10_binary_search.py
+++ week2/1. basic/10_binary_search.py
@@ -42,7 +42,19 @@ def binary_search(arr, target):
     ## 같으면 mid 반환
     ## target이 더 크면 left = mid + 1
     ## target이 더 작으면 right = mid - 1
-    pass
+    while(right >= left):
+        mid = (left + right) //2
+        if(arr[mid] == target):
+            return mid
+        
+        if(target > arr[mid]):
+            left = mid + 1
+        else:
+            right = mid - 1
+            
+        if(target > arr[right] or target < arr[left]):
+            break
+
     
     return -1
 
