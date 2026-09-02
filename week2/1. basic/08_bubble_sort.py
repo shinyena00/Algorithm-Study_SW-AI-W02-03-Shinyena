67e5c3b77ccacf41423fe183cc17549f97dc80fb
shinyena00 <syn77@pusan.ac.kr>
2026-09-02 21:43:18 +0900

    버블 sort 안 풀어져잇던 거 발견해서 풀엇음

--- week2/1. basic/08_bubble_sort.py
+++ week2/1. basic/08_bubble_sort.py
@@ -61,16 +61,20 @@ def bubble_sort_optimized(arr):
     """
     n = len(arr)
     
-    for i in range(n):
+    for i in range(n-1):
         swapped = False  # 교환 발생 여부
         
         # TODO: 내부 반복문과 교환 로직 구현
         # 교환이 발생하면 swapped = True 설정        
-        pass
+        for j in range(0, n-i-1):
+                if arr[j] > arr[j+1]:
+                    arr[j+1], arr[j] = arr[j], arr[j+1]
+                    swapped = True
         
 
         # TODO: 교환이 없으면 이미 정렬된 것이므로 break
-        pass
+        if not swapped:
+            break
 
     return arr
 
