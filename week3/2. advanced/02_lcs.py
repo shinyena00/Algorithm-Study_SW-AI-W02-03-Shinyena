b7fe0bd92ffc6c65688d93c21b1f5b19fa774721
shinyena00 <syn77@pusan.ac.kr>
2026-09-05 15:18:01 +0900

    advanced 2 완료

--- week3/2. advanced/02_lcs.py
+++ week3/2. advanced/02_lcs.py
@@ -24,7 +24,7 @@ lcs_length(s1: str, s2: str) -> int
 ▣ 힌트 (2차원 DP)
 - dp[i][j] := s1[0:i] 와 s2[0:j] 의 LCS 길이 (1-based 로 보면 편함)
 - 점화식:
-    if s1[i-1] == s2[j-1]:   dp[i][j] = dp[i-1][j-1] + 1
+    if s1[i-1] == s2 [j-1]:   dp[i][j] = dp[i-1][j-1] + 1
     else:                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
 - 초기 조건: dp[0][*] = dp[*][0] = 0
 - 최종 답은 dp[len(s1)][len(s2)] 입니다.
@@ -37,10 +37,25 @@ def lcs_length(s1: str, s2: str) -> int:
     어느 한쪽이라도 비어 있으면 0 을 반환합니다.
     """
     # TODO: 빈 문자열 처리
+    if not s1 or not s2:
+        return 0
+    
     # TODO: (len(s1)+1) x (len(s2)+1) 크기의 2차원 dp 배열을 0 으로 초기화
+    matrix = [[0] * (len(s2) +1) for _ in range(len(s1) +1 )]
+    
     # TODO: 이중 반복문으로 점화식에 따라 dp 채우기
+    for i in range(1, len(s1)+1):
+        for j in range(1, len(s2) + 1):
+            if s2[j-1] == s1[i-1]:
+              matrix[i][j] += matrix[i-1][j-1] + 1
+            else:
+              matrix[i][j] = max(matrix[i-1][j], matrix[i][j-1])
+            
+
+
+            
     # TODO: dp[len(s1)][len(s2)] 반환
-    pass
+    return matrix[len(s1)][len(s2)]
 
 
 if __name__ == "__main__":
